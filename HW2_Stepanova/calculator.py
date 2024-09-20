8041362f49ce8ecb2d28a05a69d9d3b2ebbc1425
Stepanova Valeriya <ukrainskaya49@gmail.com>
2024-09-20 11:01:21 +0300

    добавлена фукция main

--- HW2_Stepanova/calculator.py
+++ HW2_Stepanova/calculator.py
@@ -4,4 +4,18 @@ def  get_numbers():
     exp = int(input("Введите выражение: "))
     return exp
 
+def main():
+    # Получение чисел
+    exp = get_numbers()
 
+    # Запрос операции
+    operation = oper(exp)
+
+    # Вычисление
+    result = calculate(first, second, oper)
+
+    # Вывод результат
+    display(result)
+
+if __name__ == "__main__":
+    run()
